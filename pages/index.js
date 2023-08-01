2a091c6609eba17be01f00c5c12735fee38b163f
Niracha Marchetti <marchettibiw@gmail.com>
2023-08-01 12:10:39 +0200

    acreate variable for groceries in groceries.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,55 @@
+const groceries = [
+  {
+    name: "apple",
+    amount: 6,
+    category: "vegetable",
+    purchasedDate: "31-07-2023",
+    expirationDate: "07-08-2023",
+  },
+  {
+    name: "apple",
+    amount: 6,
+    category: "vegetable",
+    purchasedDate: "31-07-2023",
+    expirationDate: "07-08-2023",
+  },
+  {
+    name: "apple",
+    amount: 6,
+    category: "vegetable",
+    purchasedDate: "31-07-2023",
+    expirationDate: "07-08-2023",
+  },
+  {
+    name: "apple",
+    amount: 6,
+    category: "vegetable",
+    purchasedDate: "31-07-2023",
+    expirationDate: "07-08-2023",
+  },
+  {
+    name: "apple",
+    amount: 6,
+    category: "vegetable",
+    purchasedDate: "31-07-2023",
+    expirationDate: "07-08-2023",
+  },
+  {
+    name: "apple",
+    amount: 6,
+    category: "vegetable",
+    purchasedDate: "31-07-2023",
+    expirationDate: "07-08-2023",
+  },
+  {
+    name: "apple",
+    amount: 6,
+    category: "vegetable",
+    purchasedDate: "31-07-2023",
+    expirationDate: "07-08-2023",
+  },
+];
+
 export default function HomePage() {
   return (
     <div>
