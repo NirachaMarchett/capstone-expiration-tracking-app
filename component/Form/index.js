96456b0e9bfc7d65709625911d29cd2c6821c2e1
Niracha Marchetti <marchettibiw@gmail.com>
2023-08-04 12:38:42 +0200

    add default emoji heading

--- component/Form/index.js
+++ component/Form/index.js
@@ -34,6 +34,7 @@ export default function AddGroceryItemForm({ onAddItem }) {
               type="text"
               name="emoji"
               placeholder="➕"
+              defaultValue="➕"
             />
           </StyledHeading>
           <datalist id="emojis">
