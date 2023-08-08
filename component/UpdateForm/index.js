d6961b1338f654a824e5de84e6768d76bf9f79b2
Niracha Marchetti <marchettibiw@gmail.com>
2023-08-08 16:04:26 +0200

    minor fix on codes

--- component/UpdateForm/index.js
+++ component/UpdateForm/index.js
@@ -13,86 +13,82 @@ import {
 
 export default function UpdateForm({ defaultValues }) {
   return (
-    <>
-      <form>
-        <StyledFormField>
-          <StyledHeading>Update Item Detail</StyledHeading>
-          <StyledHeading>
-            <StyledEmojiInput
-              list="emojis"
-              id="emoji"
+    <form>
+      <StyledFormField>
+        <StyledHeading>Update Item Detail</StyledHeading>
+        <StyledHeading>
+          <StyledEmojiInput
+            list="emojis"
+            id="emoji"
+            type="text"
+            name="emoji"
+            defaultValue={defaultValues.emoji}
+          />
+        </StyledHeading>
+        <datalist id="emojis">
+          {groceryEmojis.map((groceryEmoji) => (
+            <option key={groceryEmoji} value={groceryEmoji} />
+          ))}
+        </datalist>
+        <StyledInputSection>
+          <StyledInputField>
+            <StyledLabel htmlFor="name">Item Name:</StyledLabel>
+            <StyledInput
+              id="name"
               type="text"
-              name="emoji"
-              defaultValue={defaultValues.emoji}
+              name="name"
+              defaultValue={defaultValues.name}
+              required
             />
-          </StyledHeading>
-          <datalist id="emojis">
-            {groceryEmojis.map((groceryEmoji) => (
-              <option key={groceryEmoji} value={groceryEmoji} />
-            ))}
-          </datalist>
-          <StyledInputSection>
-            <StyledInputField>
-              <StyledLabel htmlFor="name">Item Name:</StyledLabel>
-              <StyledInput
-                id="name"
-                type="text"
-                name="name"
-                defaultValue={defaultValues.name}
-                required
-              />
-            </StyledInputField>
+          </StyledInputField>
 
-            <StyledInputField>
-              <StyledLabel htmlFor="purchaseDate">Purchased Date:</StyledLabel>
-              <StyledInput
-                id="purchasedDate"
-                type="date"
-                name="purchasedDate"
-                defaultValue={defaultValues.purchasedDate}
-                required
-              />
-            </StyledInputField>
+          <StyledInputField>
+            <StyledLabel htmlFor="purchaseDate">Purchased Date:</StyledLabel>
+            <StyledInput
+              id="purchasedDate"
+              type="date"
+              name="purchasedDate"
+              defaultValue={defaultValues.purchasedDate}
+              required
+            />
+          </StyledInputField>
 
-            <StyledInputField>
-              <StyledLabel htmlFor="expirationDate">
-                Expiration Date:
-              </StyledLabel>
-              <StyledInput
-                id="expirationDate"
-                type="date"
-                name="expirationDate"
-                defaultValue={defaultValues.expirationDate}
-                required
-              />
-            </StyledInputField>
+          <StyledInputField>
+            <StyledLabel htmlFor="expirationDate">Expiration Date:</StyledLabel>
+            <StyledInput
+              id="expirationDate"
+              type="date"
+              name="expirationDate"
+              defaultValue={defaultValues.expirationDate}
+              required
+            />
+          </StyledInputField>
 
-            <StyledInputField>
-              <StyledLabel htmlFor="amount">Amount:</StyledLabel>
-              <StyledInput id="amount" type="number" name="amount" />
-            </StyledInputField>
+          <StyledInputField>
+            <StyledLabel htmlFor="amount">Amount:</StyledLabel>
+            <StyledInput id="amount" type="number" name="amount" />
+          </StyledInputField>
 
-            <StyledInputField>
-              <StyledLabel htmlFor="category">Category:</StyledLabel>
-              <StyledSelectField>
-                <StyledSelect name="category" id="category">
-                  <option value="">--Select Category--</option>
-                  <option value="fruit">Fruit</option>
-                  <option value="vegetable">Vegetable</option>
-                  <option value="food">Food</option>
-                  <option value="meat">Meat</option>
-                  <option value="protein">Protein Food</option>
-                  <option value="Dairy">Dairy</option>
-                  <option value="pastry">Pastry</option>
-                  <option value="beverages">Beverages</option>
-                  <option value="beverages">Others</option>
-                </StyledSelect>
-              </StyledSelectField>
-            </StyledInputField>
-          </StyledInputSection>
-        </StyledFormField>
-      </form>
-    </>
+          <StyledInputField>
+            <StyledLabel htmlFor="category">Category:</StyledLabel>
+            <StyledSelectField>
+              <StyledSelect name="category" id="category">
+                <option value="">--Select Category--</option>
+                <option value="fruit">Fruit</option>
+                <option value="vegetable">Vegetable</option>
+                <option value="food">Food</option>
+                <option value="meat">Meat</option>
+                <option value="protein">Protein Food</option>
+                <option value="dairy">Dairy</option>
+                <option value="pastry">Pastry</option>
+                <option value="beverages">Beverages</option>
+                <option value="others">Others</option>
+              </StyledSelect>
+            </StyledSelectField>
+          </StyledInputField>
+        </StyledInputSection>
+      </StyledFormField>
+    </form>
   );
 }
 
